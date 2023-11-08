337361f03b3e4bd1b85f86089a53855bf02c13c2
Ville Penttinen <villem.penttinen@gmail.com>
2023-11-08 10:09:34 +0200

    Add support for readonly enumerators for Span

--- src/SpanUtils/Extensions/ReadOnlySpanChunksExtensions.cs
+++ src/SpanUtils/Extensions/ReadOnlySpanChunksExtensions.cs
@@ -46,4 +46,25 @@ public static class ReadOnlySpanChunksExtensions
     /// <returns>Enumerator which yields the chunks.</returns>
     public static ReadOnlySpanChunksEnumerator<T> GetReadOnlyChunksEnumerator<T>(this ReadOnlySpan<T> source, int size, bool exact) =>
         new(source, size, exact);
+
+    /// <summary>
+    /// Enumerate read-only chunks inside the given source
+    /// </summary>
+    /// <typeparam name="T">Type of values</typeparam>
+    /// <param name="source">Source values to enumerate</param>
+    /// <param name="size">Size of each chunk</param>
+    /// <returns>Enumerator which yields the chunks.</returns>
+    public static ReadOnlySpanChunksEnumerator<T> GetReadOnlyChunksEnumerator<T>(this Span<T> source, int size) =>
+        new(source, size);
+
+    /// <summary>
+    /// Enumerate read-only chunks inside the given source
+    /// </summary>
+    /// <typeparam name="T">Type of values</typeparam>
+    /// <param name="source">Source values to enumerate</param>
+    /// <param name="size">Size of each chunk</param>
+    /// <param name="exact">Whether only chunks matching the exact size will be enumerated. Default is <c>true</c></param>
+    /// <returns>Enumerator which yields the chunks.</returns>
+    public static ReadOnlySpanChunksEnumerator<T> GetReadOnlyChunksEnumerator<T>(this Span<T> source, int size, bool exact) =>
+        new(source, size, exact);
 }
--- src/SpanUtils/Extensions/ReadOnlySpanReverseExtensions.cs
+++ src/SpanUtils/Extensions/ReadOnlySpanReverseExtensions.cs
@@ -22,4 +22,13 @@ public static class ReadOnlySpanReverseExtensions
     /// <returns>Enumerator which yields the values in reverse.</returns>
     public static ReadOnlySpanReverseEnumerator<T> GetReadOnlyReverseEnumerator<T>(this ReadOnlySpan<T> source) =>
         new(source);
+
+    /// <summary>
+    /// Enumerate the contents of the source in reverse.
+    /// </summary>
+    /// <typeparam name="T">Type of values</typeparam>
+    /// <param name="source">Source to enumerate</param>
+    /// <returns>Enumerator which yields the values in reverse.</returns>
+    public static ReadOnlySpanReverseEnumerator<T> GetReadOnlyReverseEnumerator<T>(this Span<T> source) =>
+        new(source);
 }
