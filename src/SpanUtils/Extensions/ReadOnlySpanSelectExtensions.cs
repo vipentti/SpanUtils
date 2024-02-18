8de4e40de17ae788543968ddd4f96ba4a4b05566
Ville Penttinen <villem.penttinen@gmail.com>
2024-02-18 07:30:55 +0000

    Enable recommended NET analyzers

--- src/SpanUtils/Extensions/ReadOnlySpanSelectExtensions.cs
+++ src/SpanUtils/Extensions/ReadOnlySpanSelectExtensions.cs
@@ -15,35 +15,35 @@ public static class ReadOnlySpanSelectExtensions
     /// <summary>
     /// Enumerate over the source by mapping values with the given function
     /// </summary>
-    /// <typeparam name="T">Input type</typeparam>
-    /// <typeparam name="U">Output type</typeparam>
+    /// <typeparam name="TInput">Input type</typeparam>
+    /// <typeparam name="TOutput">Output type</typeparam>
     /// <param name="source">Source to enumerate</param>
     /// <param name="selector">Function to map the input values with</param>
     /// <returns>Enumerator which maps the values contained in the source with the given selector</returns>
-    public static ReadOnlySpanSelectEnumerator<T, U> EnumerateReadOnlySelect<T, U>(
-        this T[] source,
-        Func<T, U> selector
-    ) => new(source, selector);
+    public static ReadOnlySpanSelectEnumerator<TInput, TOutput> EnumerateReadOnlySelect<
+        TInput,
+        TOutput
+    >(this TInput[] source, Func<TInput, TOutput> selector) => new(source, selector);
 
     /// <summary>
     /// Enumerate over the source by mapping values with the given function
     /// </summary>
-    /// <typeparam name="T">Input type</typeparam>
-    /// <typeparam name="U">Output type</typeparam>
+    /// <typeparam name="TInput">Input type</typeparam>
+    /// <typeparam name="TOutput">Output type</typeparam>
     /// <returns>Enumerator which maps the values contained in the source with the given selector</returns>
-    public static ReadOnlySpanSelectEnumerator<T, U> EnumerateReadOnlySelect<T, U>(
-        this ReadOnlySpan<T> source,
-        Func<T, U> selector
-    ) => new(source, selector);
+    public static ReadOnlySpanSelectEnumerator<TInput, TOutput> EnumerateReadOnlySelect<
+        TInput,
+        TOutput
+    >(this ReadOnlySpan<TInput> source, Func<TInput, TOutput> selector) => new(source, selector);
 
     /// <summary>
     /// Enumerate over the source by mapping values with the given function
     /// </summary>
-    /// <typeparam name="T">Input type</typeparam>
-    /// <typeparam name="U">Output type</typeparam>
+    /// <typeparam name="TInput">Input type</typeparam>
+    /// <typeparam name="TOutput">Output type</typeparam>
     /// <returns>Enumerator which maps the values contained in the source with the given selector</returns>
-    public static ReadOnlySpanSelectEnumerator<T, U> EnumerateReadOnlySelect<T, U>(
-        this Span<T> source,
-        Func<T, U> selector
-    ) => new(source, selector);
+    public static ReadOnlySpanSelectEnumerator<TInput, TOutput> EnumerateReadOnlySelect<
+        TInput,
+        TOutput
+    >(this Span<TInput> source, Func<TInput, TOutput> selector) => new(source, selector);
 }
